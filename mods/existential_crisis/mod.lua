23c39b34323ce91118d544bee1a75d84c5828a87
DrakeOne <124852325+DrakeOne@users.noreply.github.com>
2025-07-04 16:01:14 -0400

    Fix existential_crisis mod.lua - properly handle world initialization

--- mods/existential_crisis/mod.lua
+++ mods/existential_crisis/mod.lua
@@ -6,6 +6,7 @@ function Mod:init()
     self.bob_defeated = false
     self.times_died_to_bob = 0
     self.philosophical_level = 0
+    self.intro_played = false
 end
 
 function Mod:load(data, new_file)
@@ -14,11 +15,7 @@ function Mod:load(data, new_file)
         self.bob_defeated = data.bob_defeated or false
         self.times_died_to_bob = data.times_died_to_bob or 0
         self.philosophical_level = data.philosophical_level or 0
-    end
-    
-    -- Start with the intro cutscene
-    if new_file and Game.world then
-        Game.world:startCutscene("intro")
+        self.intro_played = data.intro_played or false
     end
 end
 
@@ -27,14 +24,29 @@ function Mod:save(data)
     data.bob_defeated = self.bob_defeated
     data.times_died_to_bob = self.times_died_to_bob
     data.philosophical_level = self.philosophical_level
+    data.intro_played = self.intro_played
     return data
 end
 
-function Mod:postInit()
+function Mod:postInit(new_file)
     -- Easter egg: if player name is Bob
     if Game.save_name and Game.save_name:upper() == "BOB" then
         Game:setFlag("identity_crisis", true)
     end
 end
+
+-- Hook into the game world when it's ready
+Kristal.callEvent(KRISTAL_EVENT.onWorldInit, function()
+    -- Start intro cutscene only once when entering the world
+    if Game.world and not Mod.intro_played then
+        Mod.intro_played = true
+        -- Use a timer to ensure everything is loaded
+        Game.world.timer:after(0.1, function()
+            if Game.world.player then
+                Game.world:startCutscene("intro")
+            end
+        end)
+    end
+end)
 
 return Mod
