32e3bfb4da83d55bab7f92a25f5c39ad78ad4b64
DrakeOne <124852325+DrakeOne@users.noreply.github.com>
2025-07-04 13:36:11 -0400

    Create void map for mod initialization

---
+++ mods/existential_crisis/scripts/world/maps/void.lua
@@ -0,0 +1,48 @@
+return {
+  version = "1.5",
+  luaversion = "5.1",
+  tiledversion = "1.8.4",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 16,
+  height = 12,
+  tilewidth = 40,
+  tileheight = 40,
+  nextlayerid = 3,
+  nextobjectid = 2,
+  backgroundcolor = { 0, 0, 0 },
+  properties = {
+    ["name"] = "The Void"
+  },
+  tilesets = {},
+  layers = {
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 2,
+      name = "markers",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      objects = {
+        {
+          id = 1,
+          name = "spawn",
+          type = "",
+          shape = "point",
+          x = 320,
+          y = 240,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        }
+      }
+    }
+  }
+}
